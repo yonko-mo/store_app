2959037595ce9e18eab79a5c599dfcc579800f17
yonko-mo <alsantel.ma@gmail.com>
2026-07-21 15:59:35 +0300

    refactor: update category services to use Api helper
    - Refactor `AllCategoriesService` to fetch categories using `Api().get()`
    - Refactor `GetCategoryService` to fetch category products using `Api().get()`

--- lib/services/all_categories_service.dart
+++ lib/services/all_categories_service.dart
@@ -1,19 +1,10 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:store_app/helper/api.dart';
 
 class AllCategoriesService {
   Future<List<dynamic>> getAllCategories() async {
-    http.Response response = await http.get(
-      Uri.parse('https://fakestoreapi.com/products/categories'),
+    List<dynamic> data = await Api().get(
+      url: 'https://fakestoreapi.com/products/categories',
     );
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
-      return data;
-    } else {
-      throw Exception(
-        'there is a problem with status code ${response.statusCode}',
-      );
-    }
+    return data;
   }
 }
